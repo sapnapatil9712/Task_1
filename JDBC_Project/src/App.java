8a2a410cdf49dd08298a7a0f4e71684643e75b17
sapnapatil9712 <sapnapatil9712@gmail.com>
2025-02-24 20:53:25 +0530

    28-Commit

---
+++ JDBC_Project/src/App.java
@@ -0,0 +1,20 @@
+
+import java.sql.*;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:mysql://localhost:3306/mydb";
+        String username = "root";
+        String password = "Sapna@2003_4";
+
+        Connection conn = DriverManager.getConnection(url, username, password);
+        System.out.println("JDBC Connected Successfully");
+
+        // String sql = "create table users(id int primary key auto_increment,name varchar(100),email varchar(100))";
+        String sql = "INSERT INTO users(name,email) VALUES ('sapna patil',sapna@gmail.com)";
+
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(sql);
+    }
+}
